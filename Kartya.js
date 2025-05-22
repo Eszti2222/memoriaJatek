f0ab6e0ed8a0ef62d8ef512bce761b1663ad8578
Eszti2222 <nagy.eszter2222@gmail.com>
2025-05-22 10:34:04 +0200

    Kartya,js megjelenit, kartyaLista, index.js new Kartya

---
+++ Kartya.js
@@ -0,0 +1,23 @@
+export default class Kartya{
+    #fajlnev="";
+    #allapot=false;//false -> nincs felfordítva
+    #divElem;
+    #imgElem;
+    #id;
+
+    constructor(id,fajlnev,szuloElem){
+        this.#id=id;
+        this.#fajlnev=fajlnev;
+        this.#divElem=szuloElem;
+        this.megjelenit();
+    }
+    megjelenit(){
+        /*egy kártya megjelenitése */
+        let html=`
+            <div class="kartya">
+            <img src="${this.#fajlnev}" alt="kép">
+            </div>
+        `
+        this.#divElem.insertAdjacentHTML("beforeend",html);
+    }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import Kartya from "./Kartya.js";
+import { kartyaLista } from "./KartyaLista.js";
+
+const kartya= new Kartya(0,kartyaLista[0],document.querySelector(".jatekter"))
